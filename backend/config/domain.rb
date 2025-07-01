12f49f4e782dc00cadcb2ac9a0033db33214173a
Ershad Kunnakkadan <ershad92@gmail.com>
2025-07-01 16:51:28 -0400

    Remove https and Nginx

--- backend/config/domain.rb
+++ backend/config/domain.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 # Simple configuration that relies on environment variables being set appropriately for each environment
-PROTOCOL = "https"
-ROOT_DOMAIN = ENV.fetch("DOMAIN")
-DOMAIN = ENV.fetch("APP_DOMAIN", ROOT_DOMAIN)
-API_DOMAIN = ENV.fetch("API_DOMAIN", ROOT_DOMAIN)
+
+ENVIRONMENT = ENV.fetch("RAILS_ENV", "development")
+
+configuration_by_environment = {
+  "development" => {
+    protocol: "http",
+    root_domain: "localhost",
+    domain: "localhost",
+    api_domain: "localhost",
+  },
+  "production" => {
+    protocol: "https",
+    root_domain: "flexile.com",
+    domain: "flexile.com",
+    api_domain: "api.flexile.com",
+  },
+}
+
+PROTOCOL = configuration_by_environment[ENVIRONMENT][:protocol] || "https"
+ROOT_DOMAIN = ENV.fetch("DOMAIN") || configuration_by_environment[ENVIRONMENT][:root_domain]
+DOMAIN = ENV.fetch("APP_DOMAIN", ROOT_DOMAIN) || configuration_by_environment[ENVIRONMENT][:domain]
+API_DOMAIN = ENV.fetch("API_DOMAIN", ROOT_DOMAIN) || configuration_by_environment[ENVIRONMENT][:api_domain]
