3ac7d191247d1077a8f4f0230d8ac65f8ffc071c
MuskanPaliwal <muskan10112002@gmail.com>
2025-07-28 22:34:26 +0530

    minor fix

--- backend/app/services/leave_company_service.rb
+++ backend/app/services/leave_company_service.rb
@@ -19,7 +19,6 @@ class LeaveCompanyService
   end
 
   private
-
     def validate_user_can_leave!
       if user_is_administrator?
         raise "Administrators cannot leave a company."
@@ -40,19 +39,19 @@ class LeaveCompanyService
       end
     end
 
-  def remove_user_roles!
-    user.company_workers.where(company: company).delete_all
-    user.company_investors.where(company: company).delete_all
-    user.company_lawyers.where(company: company).delete_all
-  end
+    def remove_user_roles!
+      user.company_workers.where(company: company).delete_all
+      user.company_investors.where(company: company).delete_all
+      user.company_lawyers.where(company: company).delete_all
+    end
 
-  def user_is_administrator?
-    user.company_administrators.exists?(company: company)
-  end
+    def user_is_administrator?
+      user.company_administrators.exists?(company: company)
+    end
 
-  def user_has_leavable_role?
-    user.company_workers.exists?(company: company) ||
-      user.company_investors.exists?(company: company) ||
-      user.company_lawyers.exists?(company: company)
-  end
+    def user_has_leavable_role?
+      user.company_workers.exists?(company: company) ||
+        user.company_investors.exists?(company: company) ||
+        user.company_lawyers.exists?(company: company)
+    end
 end
