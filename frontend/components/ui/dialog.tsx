c8bd2190b5f3832cc9ca9f2f9d35f5eeaf928fa6
nazar <gladyshinbox@outlook.com>
2025-08-06 18:19:33 +0200

    reset dialog background

--- frontend/components/ui/dialog.tsx
+++ frontend/components/ui/dialog.tsx
@@ -20,7 +20,7 @@ function DialogOverlay({ className, ...props }: React.ComponentProps<typeof Dial
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/18 md:bg-black/80",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80",
         className,
       )}
       {...props}
